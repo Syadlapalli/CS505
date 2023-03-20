5ac1edaff0b78d55bc455fa98ba51c2226abe374
Sharath <syadlapalli@my365.bellevue.edu>
2023-03-20 08:40:28 +0530

    assignment 1.3 initial commit

---
+++ TestFanApp.java
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @author Sarath
+ *
+ */
+public class TestFanApp {
+
+	public static void main(String[] args) {
+		Fan f1 = new Fan();
+		Fan f2= new Fan(2,true,8,"Blue");
+		
+		System.out.println(f1.toString());
+		System.out.println(f2.toString());
+	}
+}
